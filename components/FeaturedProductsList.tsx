7db54f5a68d32ef7421c1f174a16d2b43fd35205
afragerami <afragerami@gmail.com>
2023-07-28 21:41:38 +0330

    Refactored Featured Product list

--- components/FeaturedProductsList.tsx
+++ components/FeaturedProductsList.tsx
@@ -1,39 +1,26 @@
 import FeaturedProductCard from "./FeaturedProductCard";
 import { Box, Flex, HStack, Heading } from "@chakra-ui/react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Product } from "../hooks/useProducts";
 
 interface Props {
-  productsin: [];
+  productsin: Product[];
 }
 
 const FeaturedProductsList = ({ productsin }: Props) => {
-  let featuredProdcuts: (typeof productsin)[] = [];
+  let featuredProdcuts: Product[] = [];
 
   console.log(productsin);
 
-  productsin.forEach((element) => {
+  productsin.forEach((element: Product) => {
     if (element.featured) {
       console.log(element);
       featuredProdcuts.push(element);
     }
   });
 
-  //products.forEach((item) => {
-  // if (item.featured === true) {
-  // featuredProdcuts.push(item);
-  // }
-  //});
-
   return (
     <>
-      {/* <Heading paddingX="24px">Featured</Heading>
-      <Flex paddingX="24px">
-        <HStack w="100%" overflowX="scroll" marginBlock={3} spacing="8px">
-          {featuredProdcuts.map((element) => (
-            <FeaturedProductCard key={element.name} product={element} />
-          ))}
-        </HStack>
-      </Flex> */}
-
       <Box paddingY="16px" paddingX="24px">
         <Heading>Featured</Heading>
         <Flex>
