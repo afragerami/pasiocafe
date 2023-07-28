722321361cf8a8ba13dd802c56bcab6a105a7512
afragerami <afragerami@gmail.com>
2023-07-28 21:43:00 +0330

    Refactor CategoryTitleRow.tsx

--- components/CategoryTitleRow.tsx
+++ components/CategoryTitleRow.tsx
@@ -1,11 +1,12 @@
 import { Tr, Th, Text, Divider } from "@chakra-ui/react";
 import React from "react";
 
-const CategoryTitleRow = ({ category }) => {
+interface Props {
+  category: string;
+}
+
+const CategoryTitleRow = ({ category }: Props) => {
   return (
-    // <Tr width="100%" bg={"gray.700"}>
-    //   <Th colSpan={2}>{category}</Th>
-    // </Tr>
     <>
       <Text fontFamily="inter" fontSize="16px" fontWeight="bold" padding="24px">
         {category}
