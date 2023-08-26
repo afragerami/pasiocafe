7d8ff772e30f295804fb9cc386b9c5c7b5db0ea3
afragerami <afragerami@gmail.com>
2023-08-26 19:37:26 +0330

    Changed Theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,18 @@ import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
+const theme = {
+  semanticTokens: {
+    colors: {
+      "background.pressed.base": { default: "blue.800", _dark: "blue.300" },
+      "background.pressed.subtle": { default: "blue.300", _dark: "blue.700" },
+    },
+  },
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
