db153c44c536933d5b26fcc13500de323a969e16
afragerami <afragerami@gmail.com>
2023-08-26 21:09:49 +0330

    Change scroll on featured list

--- components/FeaturedProductsList.tsx
+++ components/FeaturedProductsList.tsx
@@ -1,6 +1,7 @@
 import FeaturedProductCard from "./FeaturedProductCard";
 import { Box, Flex, HStack, Heading } from "@chakra-ui/react";
 import { Product } from "../hooks/useProducts";
+import { useState } from "react";
 
 interface Props {
   productsin: Product[];
@@ -8,7 +9,7 @@ interface Props {
 
 const FeaturedProductsList = ({ productsin }: Props) => {
   let featuredProdcuts: Product[] = [];
-
+  const [hasScrolled, setHasScrolled] = useState(false);
   console.log(productsin);
 
   productsin.forEach((element: Product) => {
@@ -18,17 +19,35 @@ const FeaturedProductsList = ({ productsin }: Props) => {
     }
   });
 
+  const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
+    // Get the amount scrolled horizontally
+    const scrollAmount = event.currentTarget.scrollLeft;
+
+    // Check if the scroll amount is greater than or equal to 50 pixels
+    if (scrollAmount >= 24) {
+      setHasScrolled(true);
+    }
+  };
+
   return (
     <>
-      <Box paddingY="16px" paddingX="24px">
-        <Heading>Featured</Heading>
+      <Box
+        paddingY="16px"
+        paddingLeft={!hasScrolled ? "24px" : "0px"}
+        scrollPadding={0}
+        transition="padding 0.3s ease-in-out"
+      >
+        <Heading paddingBottom="16px" paddingX="24px">
+          Featured
+        </Heading>
         <Flex>
           <HStack
             w="100%"
             overflowX="scroll"
             className="FeaturedProducts"
-            marginBlock={3}
+            //marginBlock={3}
             spacing="8px"
+            onScroll={handleScroll}
           >
             {featuredProdcuts.map((element) => (
               <FeaturedProductCard key={element.name} product={element} />
